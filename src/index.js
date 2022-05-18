334940c7ab5a67c130f3f42f3a1a361dd25b7aa9
Elwood-P <info@pjlittlewood.co.uk>
2022-05-18 13:12:07 +0100

    Finish js basic functionality

--- src/index.js
+++ src/index.js
@@ -1,42 +1,92 @@
 // Data init
-let timer = {
-  timeTotal: 60,
-  timeUsed: 10,
+let timerData = {
+  timeTotal: 3600, // 60m
+  timeUsed: 0,
 };
 
 // Clock
 const clockTimeLeft = document.querySelector('.clock__timeleft');
 const clockFace = document.querySelector('.clock__face');
+// Forms & control
+const formSetup = document.querySelector('.form__setup');
+const formRunning = document.querySelector('.form__running');
+const controlsTimeleft = document.querySelector('.controls__timeleft');
+const inputTimeTotal = document.getElementById('input--time-total');
+const inputTimeUsed = document.getElementById('input--time-used');
 // Buttons
 const btnStart = document.getElementById('btn--start');
 const btnPause = document.getElementById('btn--pause');
+const btnPlay = document.getElementById('btn--play');
 const btnCancel = document.getElementById('btn--cancel');
-// Forms
-const inputTimeTotal = document.getElementById('input--time-total');
-const inputTimeUsed = document.getElementById('input--time-used');
 
 //Event Listeners
-inputTimeTotal.addEventListener('input', updateTimer);
-inputTimeUsed.addEventListener('input', updateTimer);
+inputTimeTotal.addEventListener('input', updateTimerData);
+inputTimeUsed.addEventListener('input', updateTimerData);
+btnStart.addEventListener('click', (e) => btnStartHandler(e));
+btnPause.addEventListener('click', (e) => btnPauseHandler(e));
+btnPlay.addEventListener('click', (e) => btnPlayHandler(e));
+btnCancel.addEventListener('click', (e) => btnCancelHandler(e));
 
 function init() {
-  inputTimeTotal.value = timer.timeTotal;
-  inputTimeUsed.value = timer.timeUsed;
-  updateClockFace();
+  inputTimeTotal.value = timerData.timeTotal / 60;
+  inputTimeUsed.value = timerData.timeUsed / 60;
+  updateClock();
 }
 
-function updateClockFace() {
-  const pct = 100 - ((timer.timeTotal - timer.timeUsed) / timer.timeTotal) * 100;
+function updateClock() {
+  const { timeTotal, timeUsed } = timerData;
+  const timeLeft = timeTotal - timeUsed;
+  const minsLeftFloor = Math.floor(timeLeft / 60);
+  const minsLeftCeil = Math.ceil(timeLeft / 60);
+  const secsLeft = timeLeft - minsLeftFloor * 60;
+  const pct = 100 - (timeLeft / timeTotal) * 100;
+
   clockFace.style.setProperty('--pct', pct);
+  clockTimeLeft.innerHTML = minsLeftCeil;
+  controlsTimeleft.innerHTML = `${minsLeftFloor}m ${secsLeft}s left of ${timeTotal / 60}m`;
 }
 
-function updateTimer() {
-  timer.timeTotal = inputTimeTotal.value;
-  timer.timeUsed = inputTimeUsed.value;
-  console.log(timer.timeTotal);
-  updateClockFace();
+function updateTimerData() {
+  timerData.timeTotal = inputTimeTotal.value * 60;
+  timerData.timeUsed = inputTimeUsed.value * 60;
+  updateClock();
+}
+
+function btnStartHandler(e) {
+  e.preventDefault();
+  formSetup.classList.toggle('hidden');
+  formRunning.classList.toggle('hidden');
+  startTimer();
+}
+
+function btnPauseHandler(e) {
+  e.preventDefault();
+  clearInterval(timer);
+  e.target.classList.toggle('hidden');
+  btnPlay.classList.toggle('hidden');
+}
+
+function btnPlayHandler(e) {
+  e.preventDefault();
+  startTimer();
+  e.target.classList.toggle('hidden');
+  btnPause.classList.toggle('hidden');
+}
+
+function btnCancelHandler(e) {
+  e.preventDefault();
+  if (window.confirm("Do you really want to cancel the current timer?")) {
+    location.reload();
+  }
+}
+
+function startTimer() {
+  timer = setInterval(timerCountdown, 1000);
+  function timerCountdown() {
+    timerData.timeUsed++;
+    console.log(timerData.timeUsed);
+    updateClock();
+  }
 }
 
 init();
-
-
